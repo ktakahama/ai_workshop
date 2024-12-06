a235c0743be08d2f4ae42717f15b7a6970fb4262
Kotaro Takahama <kotaro.takahama@septeni-incubate.co.jp>
2024-12-06 15:16:45 +0900

    llmush

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
-  experimental: {
-    optimizeCss: true,
-  },
-  // assetPrefixを使用している場合は削除するか、適切なパスに修正
-  // assetPrefix: process.env.NODE_ENV === 'production' ? '/your-prefix' : '',
+  output: "standalone"
 };
 
 module.exports = nextConfig;
